d240085b983100f4f364076cd29ac923cb54f20a
Miguel Fernando <get@miguelfernando.com>
2016-09-08 08:59:51 -0400

    Added user-task association, user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'about' => "pages#about"
   get 'contact' => "pages#contact"
   get 'gallery' => "pages#gallery"
+  get 'tasks' => "tasks"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
